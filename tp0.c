048ea1613dd6230d94992c2d404d6470503990cf
mvbattan <mvbattan@gmail.com>
2017-03-25 23:32:41 -0300

    Parsing arguments. Handling option errors.

--- tp0.c
+++ tp0.c
@@ -2,7 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include <getopt.h>
+
+struct program_options {
+  FILE* input_file;
+  FILE* output_file;
+  bool should_decode;
+};
 
 void print_help() {
   // The next function call is unindented, in order to preserve tabs and spaces
@@ -101,16 +108,17 @@ void decode(char* encoded_input, char* output) {
 }
 
 // Based on https://www.gnu.org/software/libc/manual/html_node/Example-of-Getopt.html#Example-of-Getopt
-void parse_options(int argc, char** argv) {
+void parse_options(int argc, char** argv, struct program_options* options) {
   int seen_option;
   opterr = 0;
+
   while (true) {
     static struct option long_options[] = {
       {"version", no_argument,       NULL, 'V'},
       {"help",    no_argument,       NULL, 'h'},
       {"input",   required_argument, NULL, 'i'},
       {"output",  required_argument, NULL, 'o'},
-      {"action",  optional_argument, NULL, 'a'}
+      {"action",  required_argument, NULL, 'a'}
     };
 
     int option_index = 0;
@@ -118,7 +126,7 @@ void parse_options(int argc, char** argv) {
     seen_option = getopt_long(
       argc,
       argv,
-      "Vhi:o:a::",
+      "Vhi:o:a:",
       long_options,
       &option_index
     );
@@ -137,24 +145,39 @@ void parse_options(int argc, char** argv) {
         break;
 
       case 'i':
-        printf ("option -i with value `%s'\n", optarg);
+        options->input_file = fopen(optarg, "r");
+        if (!options->input_file) {
+          fprintf(stderr, "Filename Error: Cannot open %s to read.\n", optarg);
+          exit(1);
+        }
         break;
 
       case 'o':
-        printf ("option -o with value `%s'\n", optarg);
+        options->output_file = fopen(optarg, "w");
+        if (!options->output_file) {
+          fprintf(stderr, "Filename Error: Cannot open %s to write.\n", optarg);
+          exit(2);
+        }
         break;
 
       case 'a':
-        printf ("option -a with value `%s'\n", optarg);
+        if (strcmp(optarg, "encode") && strcmp(optarg, "decode")) {
+          fprintf(stderr, "Action Error: %s is not a valid action.\n", optarg);
+          exit(3);
+        }
+        options->should_decode = !!strcmp(optarg, "encode");
         break;
 
       case '?':
         if (optopt == 'i' || optopt == 'o')
-          fprintf (stderr, "Option -%c requires an filename argument.\n", optopt);
+          fprintf(stderr, "Option -%c requires an filename argument.\n", optopt);
+        else if (optopt == 'a')
+          fprintf(stderr, "Option -%c requires either 'decode' or 'encode' options.\n", optopt);
         else if (isprint (optopt))
-          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+          fprintf(stderr, "Unknown option `-%c'.\n", optopt);
         else
-          fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+          fprintf(stderr, "Unknown option character `\\x%x'.\n", optopt);
+        exit(4);
         break;
 
       default:
@@ -163,15 +186,17 @@ void parse_options(int argc, char** argv) {
   }
 
   if (optind < argc) {
-    printf ("non-option ARGV-elements: ");
-    while (optind < argc) printf ("%s ", argv[optind++]);
-    putchar ('\n');
+    printf("non-option ARGV-elements: ");
+    while (optind < argc) printf("%s ", argv[optind++]);
+    putchar('\n');
   }
 }
 
 int main (int argc, char** argv) {
 
-    parse_options(argc, argv);
+    struct program_options options = { stdin, stdout, false };
+
+    parse_options(argc, argv, &options);
 
     char input[] = {'M', 'a', 'n', '\0'};
     char encoded_output[4] = {0};
@@ -195,5 +220,10 @@ int main (int argc, char** argv) {
     printf("\n");
 
     printf("%s\n", decoded_input);
+
+    printf("%d\n", fclose(options.input_file));
+    printf("%d\n", options.output_file == stdout ? 0 : fclose(options.output_file));
+    printf("%d \n", (int) options.should_decode);
+
     return 0;
 }
